d972f46ce0754aa31462486ea0e2df4f211c95ac
Mandy Gu <32079152+happilyeverafter95@users.noreply.github.com>
2021-02-20 13:43:08 -0500

    Followup (#2)
    * add stories and small follow up
    
    * fix laptop suggestion
    
    * no more platform
    
    * followup

--- actions/actions.py
+++ actions/actions.py
@@ -6,15 +6,12 @@ from rasa_sdk.events import FollowupAction, Restarted, SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 
 
-def select_laptop(price: int, platform: Optional[str], purpose: Optional[str],
-                  brand: Optional[List[str]]) -> List[Dict[str, str]]:
+def select_laptop(price: int, purpose: Optional[str], brand: Optional[List[str]]) -> List[Dict[str, str]]:
     with open('actions/laptops.json') as f:
         laptops = json.load(f)
     laptops = [x for x in laptops if x['price'] <= price]
-    if platform and 'no preference' not in platform:
-        laptops = [x for x in laptops if x['platform'] == platform]
     if purpose:
-        laptops = [x for x in laptops if x['purpose'] == purpose]
+        laptops = [x for x in laptops if x['purpose'].lower() == purpose]
     if brand and 'no preference' not in brand:
         laptops = [x for x in laptops if x['brand'] in brand]
     return laptops
@@ -36,13 +33,8 @@ class ActionSelectPrice(Action):
     def name(self) -> Text:
         return 'action_select_upper_price'
 
-    def validate_price(self, message: str) -> bool:
-        numerical_values = [int(s) for s in message.split() if s.isdigit()]
-        return len(numerical_values) >= 1
-
     def extract_price(self, message: str) -> int:
-        # TODO: dedup methods
-        numerical_values = [int(s) for s in message.split() if s.isdigit()]
+        numerical_values = [int(s) for s in message.replace('$', '').split() if s.isdigit()]
         if len(numerical_values) >= 1:
             return numerical_values[0]
         else:
@@ -52,26 +44,14 @@ class ActionSelectPrice(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         upper_price = tracker.latest_message['text']
-        if self.validate_price(upper_price):
-            extracted_price = self.extract_price(upper_price)
-            dispatcher.utter_message(text=f'I will look for a laptop under ${upper_price}.')
+        extracted_price = self.extract_price(upper_price)
+        if extracted_price != -1:
+            dispatcher.utter_message(text=f'I will look for a laptop under ${extracted_price}.')
             dispatcher.utter_message(template='utter_purpose')
             return [SlotSet('upper_price', extracted_price)]
         else:
             dispatcher.utter_message(template='utter_default')
             return []
-
-
-class ActionSelectPlatform(Action):
-    def name(self) -> Text:
-        return 'action_select_platform'
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        platform = tracker.slots['platform']
-        dispatcher.utter_message(template='utter_ask_other_requirements')
-        return [SlotSet('platform', platform), FollowupAction('action_recommend_laptop')]
 
 
 class ActionSelectBrand(Action):
@@ -85,10 +65,9 @@ class ActionSelectBrand(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         brands = tracker.latest_message['text']
-        dispatcher.utter_message(template='utter_platform')
         if brands.lower().strip() != 'no preference':
-            return [SlotSet('brand', self.reformat_brands(brands))]
-        return [SlotSet('brand', [])]
+            return [SlotSet('brand', self.reformat_brands(brands)), FollowupAction('action_recommend_laptop')]
+        return [SlotSet('brand', []), FollowupAction('action_recommend_laptop')]
 
 
 class ActionSelectPurpose(Action):
@@ -110,8 +89,7 @@ class ActionRecommendLaptop(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        laptops = select_laptop(tracker.slots['upper_price'], tracker.slots['platform'],
-                                tracker.slots['purpose'], tracker.slots['brand'])
+        laptops = select_laptop(tracker.slots['upper_price'], tracker.slots['purpose'], tracker.slots['brand'])
         if laptops == []:
             dispatcher.utter_message(template='utter_no_recommendations')
         else:
@@ -119,7 +97,7 @@ class ActionRecommendLaptop(Action):
             for laptop in laptops[:3]:
                 dispatcher.utter_message(
                     text=f'{laptop["name"]} (${laptop["price"]})-- {laptop["description"]}.')
-        return []
+        return [FollowupAction('action_goodbye')]
 
 
 class ActionGoodbye(Action):
