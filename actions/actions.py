94d703ef5a52f473092748e1eae6e9126b4d7a65
Mandy <mandygu925@gmail.com>
2021-02-20 13:38:25 -0500

    fix laptop suggestion

--- actions/actions.py
+++ actions/actions.py
@@ -11,7 +11,7 @@ def select_laptop(price: int, platform: Optional[str], purpose: Optional[str],
     with open('actions/laptops.json') as f:
         laptops = json.load(f)
     laptops = [x for x in laptops if x['price'] <= price]
-    if platform and 'no preference' not in platform:
+    if platform:
         laptops = [x for x in laptops if x['platform'].lower() == platform]
     if purpose:
         laptops = [x for x in laptops if x['purpose'].lower() == purpose]
@@ -36,12 +36,7 @@ class ActionSelectPrice(Action):
     def name(self) -> Text:
         return 'action_select_upper_price'
 
-    def validate_price(self, message: str) -> bool:
-        numerical_values = [int(s) for s in message.replace('$', '').split() if s.isdigit()]
-        return len(numerical_values) >= 1
-
     def extract_price(self, message: str) -> int:
-        # TODO: dedup methods
         numerical_values = [int(s) for s in message.replace('$', '').split() if s.isdigit()]
         if len(numerical_values) >= 1:
             return numerical_values[0]
@@ -52,26 +47,14 @@ class ActionSelectPrice(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         upper_price = tracker.latest_message['text']
-        if self.validate_price(upper_price):
-            extracted_price = self.extract_price(upper_price)
-            dispatcher.utter_message(text=f'I will look for a laptop under ${upper_price}.')
+        extracted_price = self.extract_price(upper_price)
+        if extracted_price != -1:
+            dispatcher.utter_message(text=f'I will look for a laptop under ${extracted_price}.')
             dispatcher.utter_message(template='utter_purpose')
             return [SlotSet('upper_price', extracted_price)]
         else:
             dispatcher.utter_message(template='utter_default')
             return []
-
-
-class ActionSelectPlatform(Action):
-    def name(self) -> Text:
-        return 'action_select_platform'
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        platform = tracker.slots['platform']
-        dispatcher.utter_message(template='utter_ask_other_requirements')
-        return [SlotSet('platform', platform), FollowupAction('action_recommend_laptop')]
 
 
 class ActionSelectBrand(Action):
@@ -85,10 +68,9 @@ class ActionSelectBrand(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         brands = tracker.latest_message['text']
-        dispatcher.utter_message(template='utter_platform')
         if brands.lower().strip() != 'no preference':
-            return [SlotSet('brand', self.reformat_brands(brands))]
-        return [SlotSet('brand', [])]
+            return [SlotSet('brand', self.reformat_brands(brands)), FollowupAction('action_recommend_laptop')]
+        return [SlotSet('brand', []), FollowupAction('action_recommend_laptop')]
 
 
 class ActionSelectPurpose(Action):
@@ -110,8 +92,7 @@ class ActionRecommendLaptop(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        laptops = select_laptop(tracker.slots['upper_price'], tracker.slots['platform'],
-                                tracker.slots['purpose'], tracker.slots['brand'])
+        laptops = select_laptop(tracker.slots['upper_price'], tracker.slots['purpose'], tracker.slots['brand'])
         if laptops == []:
             dispatcher.utter_message(template='utter_no_recommendations')
         else:
